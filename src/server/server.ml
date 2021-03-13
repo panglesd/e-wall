c25ff3ee45a6a8a9f28e552643230eebf934536a
Paul-Elliot Anglès d'Auriac <peada@free.fr>
2021-03-13 23:12:54 +0100

    corrected digestif problem

--- src/server/server.ml
+++ src/server/server.ml
@@ -3,7 +3,11 @@ open Tyxml.Html
 open Model
 
 let () = Stdlib.Random.self_init ()
-   
+let get_rand_id () =
+  let _ = Stdlib.Random.bits () in
+  Uuidm.to_string @@ Uuidm.v4_gen (Stdlib.Random.get_state()) ()
+  
+       
 let myTitle = title (txt ("e-wall"))
 let div = div ~a:[a_id "app"][]
 let link = link ~rel:[`Stylesheet]  ~href:"ewall.css" ()
@@ -34,35 +38,43 @@ let get_all_panels _req =
 let getHold req =
   let idHold =  Router.param req "idHold" in
   let id = Uuidm.to_string @@ Uuidm.v4_gen (Stdlib.Random.get_state()) () in
-  let somePanel = Panel.make ~id ~name:"Un panneau quelconque" ~filename:"panel.png" () in
+  let somePanel = Panel.make ~id ~name:("Un panneau quelconque d'id "^id) ~filename:"panel.png" () in
   let someHold = Hold.make ~id:idHold ~panel:(somePanel) (* ~polygone:[(0,0);(1,1)] *) ~position:(10,10) ~size:10 ~name:"exemple de prise" in
   Response.of_json (Hold.yojson_of_t someHold)
   |> Lwt.return
   
 let getRoute req =
   let idRoute =  Router.param req "idRoute" in
-  let somePanel = Panel.make ~id:"0" ~name:"Un panneau quelconque" ~filename:"panel.png" () in
+  let id = Uuidm.to_string @@ Uuidm.v4_gen (Stdlib.Random.get_state()) () in
+  let somePanel = Panel.make ~id:idRoute ~name:"Un panneau quelconque" ~filename:"panel.png" () in
   let someHold1 = Hold.make ~id:"1" ~panel:(somePanel) (* ~polygone:[(0,0);(1,1)] *) ~position:(10,10) ~size:10 ~name:"exemple de prise" in
   let someHold2 = Hold.make ~id:"2" ~panel:(somePanel) (* ~polygone:[(0,0);(1,2)] *) ~position:(10,10) ~size:10 ~name:"exemple de prise" in
   let someRoute = Route.make
-                    ~id:idRoute
-                    ~name:"Papillote"
+                    ~id
+                    ~name:("Papillote d'id "^id)
                     ~holds:[someHold1; someHold2]
                     ~feet:Route.Feet.All () in
   Response.of_json (Route.yojson_of_t someRoute)
   |> Lwt.return
 
 let get_all_routes _req =
-  let idRoute =  "1" in
+  (* let idRoute =  "1" in *)
+  let id = Uuidm.to_string @@ Uuidm.v4_gen (Stdlib.Random.get_state()) () in
   let somePanel = Panel.make ~id:"0" ~name:"Un panneau quelconque" ~filename:"panel.png" () in
   let someHold1 = Hold.make ~id:"1" ~panel:(somePanel) (* ~polygone:[(0,0);(1,1)] *) ~position:(10,10) ~size:10 ~name:"exemple de prise 1" in
   let someHold2 = Hold.make ~id:"2" ~panel:(somePanel) (* ~polygone:[(0,0);(1,2)] *) ~position:(10,10) ~size:10 ~name:"exemple de prise 2" in
   let someRoute = Route.make
-                    ~id:idRoute
-                    ~name:"Papillote"
+                    ~id
+                    ~name:("Papillote d'id "^id)
                     ~holds:[someHold1; someHold2]
                     ~feet:Route.Feet.All () in
-  Response.of_json (Route.yojson_of_t_list [someRoute;someRoute])
+  let id = Uuidm.to_string @@ Uuidm.v4_gen (Stdlib.Random.get_state()) () in
+  let someRoute2 = Route.make
+                    ~id
+                    ~name:("Papillote 2 d'id "^id)
+                    ~holds:[someHold1; someHold2]
+                    ~feet:Route.Feet.All () in
+  Response.of_json (Route.yojson_of_t_list [someRoute;someRoute2])
   |> Lwt.return
 
  
