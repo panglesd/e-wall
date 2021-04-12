485558725987f183d1aaa7b3888f8d9a8f853d85
Paul-Elliot Anglès d'Auriac <peada@free.fr>
2021-04-12 19:50:49 +0200

    corrected bug with holds too early

--- src/web-app/views/layout.ml
+++ src/web-app/views/layout.ml
@@ -1,16 +1,18 @@
 open Tyxml_lwd.Html
-   
+open Lwt.Syntax
+
 let make () =
   let open Logic.Main_logic in
-  let () = ignore @@ update_panel_list ();
-           ignore @@ update_route_list ();
-           ignore @@ update_current_holds ();
-  in
   let panels_div  = Panel_view.make_panel_list_div all_panels_var current_panel_var in
   let main_panel_div  = Panel_view.make_main_panel_div current_panel_var current_holds_var in
   let routes_div  = Route_view.make_route_list_div all_routes_var in
   let route_info_div  = Route_view.make_current_route_div current_route_var in
   let panel_form = Panel_view.make_panel_form () (* panel_form_var *) in
+  let _ = let* _ = update_panel_list () in
+           let* _ = update_route_list () in
+           let+ _ = update_current_holds () in
+           ()
+  in
   
   div ~a:[a_class (Lwd.pure ["app-root"])] [
       div ~a:[a_class (Lwd.pure ["panels-root"])] [
